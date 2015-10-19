d6e481a5e72f09d392d5e388ebf04cb6db6b2808
saxer <15201280641@qq.com>
2015-10-19 11:29:12 +0800

    add subscribe

--- lib/kuaidi100_rails.rb
+++ lib/kuaidi100_rails.rb
@@ -2,12 +2,16 @@ require "kuaidi100_rails/version"
 require 'open-uri'
 require 'net/http'
 require 'json'
+require 'faraday'
 
 module Kuaidi100Rails
   API_URL = 'http://api.kuaidi100.com/api'
+  SUBSCRIBE_URL = 'http://www.kuaidi100.com/poll'
 
   class << self
     attr_accessor :API_KEY
+    attr_accessor :POLL_KEY
+    attr_accessor :POLL_CALLBACK_URL
   end
 
   module_function
@@ -21,6 +25,43 @@ module Kuaidi100Rails
       res = Net::HTTP.get(url)
       hash = SHash.new(JSON.parse(res))
       p hash
+    rescue Exception => e
+      puts "error #{e}"
+    end
+  end
+
+  #subscribe express
+  #===Parameters==
+  #+com+ company code
+  #+nu+ express number
+  #+from+ start from address
+  #+to+ destination address
+  #+salt+ random signature string
+  def subscribe(com, nu, from, to, salt=nil)
+    begin
+      body = {
+          company: com,
+          number: nu,
+          from: from,
+          to: to,
+          key: Kuaidi100Rails.POLL_KEY,
+          parameters: {
+              callbackurl: Kuaidi100Rails.POLL_CALLBACK_URL,
+              salt: salt,
+              resultv2: '1'
+          }
+      }
+      p body
+
+      response = Faraday.new(:url => SUBSCRIBE_URL).post do |req|
+        req.headers['Content-Type'] = 'application/json'
+        req.params[:schema] = 'json'
+        req.params[:param] = body.to_json
+      end
+
+      hash = SHash.new(JSON.parse(response.body))
+      p hash
+
     rescue Exception => e
       puts "error #{e}"
     end
--- spec/kuaidi100_rails_spec.rb
+++ spec/kuaidi100_rails_spec.rb
@@ -3,10 +3,12 @@ require 'kuaidi100_rails'
 describe Kuaidi100Rails do
 
   it 'crawl' do
-    Kuaidi100Rails.API_KEY='121312312'
-    Kuaidi100Rails.crawl('yuantong', '')
+    Kuaidi100Rails.crawl('yuantong', 'd00037098973')
+    p Kuaidi100Rails::SHash.new({'a' => 0, 'b' => ['0', 1, 2], 'c' => [{'a' => ['12', 4, {'a' => 'b', 'saxer' => [1, 2, 3, 6]}]}]})
+  end
 
+  it 'subscribe' do
 
-    p Kuaidi100Rails::SHash.new({'a' => 0, 'b' => ['0', 1, 2], 'c' => [{'a' => ['12', 4, {'a'=> 'b','saxer'=>[1,2,3,6]}]}]})
+    Kuaidi100Rails.subscribe('yuantong', 'd00025359556', '北京市', '湖北省十堰市丹江口市')
   end
 end

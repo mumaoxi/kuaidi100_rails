44ee035ef4e7f23f29ffe23e97c9103de0750345
saxer <15201280641@qq.com>
2015-10-19 15:00:27 +0800

    version update

--- spec/kuaidi100_rails_spec.rb
+++ spec/kuaidi100_rails_spec.rb
@@ -3,12 +3,14 @@ require 'kuaidi100_rails'
 describe Kuaidi100Rails do
 
   it 'crawl' do
+    Kuaidi100Rails.API_KEY='123456'
     Kuaidi100Rails.crawl('yuantong', 'd00037098973')
     p Kuaidi100Rails::SHash.new({'a' => 0, 'b' => ['0', 1, 2], 'c' => [{'a' => ['12', 4, {'a' => 'b', 'saxer' => [1, 2, 3, 6]}]}]})
   end
 
   it 'subscribe' do
-
-    Kuaidi100Rails.subscribe('yuantong', 'd00025359556', '北京市', '湖北省十堰市丹江口市')
+    Kuaidi100Rails.POLL_CALLBACK_URL='http://test.api.com/v2/kuaidis/callback'
+    Kuaidi100Rails.POLL_KEY = '123456'
+    Kuaidi100Rails.subscribe('yuantong', 'd00025359570', '北京市', '江苏省镇江市丹阳市')
   end
 end
